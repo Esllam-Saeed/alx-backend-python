3af3ccd79a5e24c703e4ac5faaac7b2bb54403e7
Esllam-Saeed <108064795+Esllam-Saeed@users.noreply.github.com>
2024-03-14 20:11:33 +0200

    Update 0-add.py

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
-'''Task 1's module.
+''' Description: type-annotated function add that takes a float a and a float b
+                 as arguments and returns their sum as a float
+    Parameters: a: float
+                b: float
 '''
 
 
-def concat(str1: str, str2: str) -> str:
-    '''Concatenates two strings.
-    '''
-    return str1 + str2
+def add(a: float, b: float) -> float:
+    ''' Return sum of a and b. '''
+    return a + b
